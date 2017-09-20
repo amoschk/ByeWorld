81b44c7abbc9a16f7103672560a31d137e8963a1
amoschk <amoschk@gmail.com>
2017-09-20 14:57:09 +0800

    asda

--- src/helloworld/alpha.java
+++ src/helloworld/alpha.java
@@ -14,5 +14,12 @@ public class alpha {
         public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HEYYA");
+        // TODO code application logic here
+        System.out.println("HEYYA");
+        // TODO code application logic here
+        System.out.println("HEYYA");
+        // TODO code application logic here
+        System.out.println("HEYYA");
+        
     }
 }
